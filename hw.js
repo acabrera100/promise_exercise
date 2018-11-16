99b6c51434cb39392cff62af264addce708dbe67
Andres.C <andrescabrera@pursuit.org>
2018-11-16 15:26:35 -0500

    now fetch time

--- hw.js
+++ hw.js
@@ -1,25 +1,44 @@
-  function testNum(num) {
-    return new Promise((resolve, reject) => {
-      resolve(num > 10);
-    }).then(boo=>{
-    console.log(boo);
-  })
-}
-testNum(100)
+//   function testNum(num) {
+//     return new Promise((resolve, reject) => {
+//       resolve(num > 10);
+//     }).then(boo=>{
+//     console.log(boo);
+//   })
+// }
+// testNum(100)
 // ------------------------------------
 //
-// function whisperShout (str) {
+// function whisperShout(str) {
+//   return new Promise(function(resolve, reject) {
+//     console.log(str.toLowerCase());
 //
-//  return new Promise (function(resolve, reject) {
-//
-//   console.log(str.toLowerCase());
-//
-//   setTimeout(() => resolve(str), 3000);
-//
-//
-//  }).then(result => {
-//   console.log(result.toUpperCase());
-//  })
+//     setTimeout(() => resolve(str), 3000);
+//   }).then(result => {
+//     console.log(result.toUpperCase());
+//   });
 // }
-// whisperShout("ok then what is going on?")
-// ----------------------------------------
+// whisperShout("ok then what is going on?");
+
+// ------------------------------------
+function waitFact(arr) {
+  let ind = 0;
+  let interval = setInterval(() => {
+    if (ind < arr.length) {
+      new Promise((resolve, reject) => {
+        console.log(arr[ind]);
+        ind++;
+        resolve();
+      });
+    } else {
+      clearInterval(interval);
+    }
+  }, 200);
+}
+
+waitFact([3,4,5])
+
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
